8d0acb89238a69f2ad10795b370a9bf7e9d1c07c
aiueoxyz <k023g0028@g.neec.ac.jp>
2024-04-12 12:19:58 +0900

    ブランチの作成

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2D_06_クラモチ_シュンスケ_AL3");
+	win->CreateGameWindow(L"LE2D_06_クラモチ_シュンスケ_AL3_1_2");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
