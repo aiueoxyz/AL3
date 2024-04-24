f233856a8db7b8fbff3247b568936ad21a0645a7
aiueoxyz <k023g0028@g.neec.ac.jp>
2024-04-24 17:26:24 +0900

    02_01_確認課題

---
+++ DirectXGame/Player.cpp
@@ -0,0 +1,24 @@
+#include "Player.h"
+#include <cassert>
+
+void Player::Initialize(Model* model, uint32_t textureHandle, ViewProjection* viewProjection) {
+	// NULLポインタチェック
+	assert(model);
+
+	// 引数の内容をメンバ変数に記録
+	model_ = model;
+	textureHandle_ = textureHandle;
+	viewProjection_ = viewProjection;
+
+	worldTransform_.Initialize();
+}
+
+void Player::Update() {
+	// 行列を定数バッファに転送
+	worldTransform_.TransferMatrix();
+}
+
+void Player::Draw() {
+	// 3Dモデルを描画
+	model_->Draw(worldTransform_, *viewProjection_, textureHandle_);
+}
